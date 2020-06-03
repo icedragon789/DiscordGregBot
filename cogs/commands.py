d464589d7d7538236814abb92592b9153ddc3fa5
icedragon789 <icedragon072@gmail.com>
2020-06-02 22:48:27 -0400

    reddit memez lol

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,5 +1,8 @@
 import discord
 import datetime
+import praw
+import random
+from datetime import datetime
 from discord.ext import commands
 
 class Commands(commands.Cog):
@@ -7,18 +10,25 @@ class Commands(commands.Cog):
     def __init__(self,client):
         self.client = client
 
-    #Events
-    # @commands.Cog.listener()
-    # async def on_ready(self):
-    #     await client.change_presence(status=discord.Status.idle, activity=discord.Game('Operating Systems'))
-    #     print('Bot is online')
-
     #Commands
     @commands.command(pass_context = True, aliases=['time','date','ping'])
     async def dt(self,ctx):
-        data = datetime.datetime.now()
-        await ctx.send('Tonight is: {}'.format(data))
+        now = datetime.now()
+        date = now.strftime("%m/%d/%Y")
+        time = now.strftime("%H:%M:%S")
 
+        await ctx.send("Tonight's date is: {0} and the time is {1}".format(date,time))
+
+    @commands.command()
+    async def meme(self,ctx, subreddit):
+        reddit = praw.Reddit(client_id="",
+            client_secret="",
+            user_agent="")
+        memes_submissions = reddit.subreddit(subreddit).hot()
+        post_to_pick = random.randint(1,10)
+        for i in range(0,post_to_pick):
+            submission = next(x for x in memes_submissions if not x.stickied)
+        await ctx.send(submission.url)
 
 def setup(client):
     client.add_cog(Commands(client))
